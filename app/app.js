347dd69e5570f16695ddeb2f7ae4f83c12f45a76
Edward R <edwardrz@users.noreply.github.com>
2017-08-31 22:14:36 -0500

    upated

--- app/app.js
+++ app/app.js
@@ -1,2 +1,17 @@
 "use strict";
 
+var app = angular.module("mushroomApp", ['ngRoute']);
+
+app.config(function($routeProvider){
+
+	$routeProvider.
+	when('/', {
+		templateUrl: 'partials/greeting.html'
+	}).
+	when('/showall', {
+		templateUrl: 'partials/flavorlist.html',
+		controller: "mushroomCtrl"
+	}).
+	otherwise('/');
+
+});
