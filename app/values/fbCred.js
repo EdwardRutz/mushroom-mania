3c6ef546a3fa5dadc6081bd57f11c5365cc4f283
Edward R <edwardrz@users.noreply.github.com>
2017-09-05 15:42:30 -0500

    removed fbCred.js

--- app/values/fbCred.js
+++
@@ -1,9 +0,0 @@
-// "use strict";
-
-//   // Initialize Firebase
-//   var config = {
-//     apiKey: "AIzaSyDato9iaXOsDqvLPLGWTndmzSOOpQl3I6I",
-//     authDomain: "mushroommania-4359b.firebaseapp.com",
-//     databaseURL: "https://mushroommania-4359b.firebaseio.com"
-//   };
-//   firebase.initializeApp(config);
